d1aded05e4a6f03ac5bfb2d4402d13fd89e875e0
Mykhailo525 <mtrindak@gmail.com>
2023-01-20 19:47:57 +0200

    First commit

--- src/components/Rocket/rocket.js
+++ src/components/Rocket/rocket.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Rocket = ({rocket}) => {
-    const{mission_name,launch_year,mission_patch_small}=rocket
+    const{mission_name,launch_year,links}=rocket
     return (
         <div>
             <div>mission_name - {mission_name}</div>
             <div>launch_year - {launch_year}</div>
-            <img src={rocket.links.mission_patch_small} alt={mission_name}/>
+            <img src={links.mission_patch_small} alt={mission_name}/>
         </div>
     );
 };
