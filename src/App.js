95509b4cc1fdeaf75d5394771ab66e131c41177a
Mykhailo525 <mtrindak@gmail.com>
2023-01-28 17:49:41 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import  {Routes,Route} from "react-router-dom"
 import {AlbumsPage, CommentsPage, HomePage, TodosPage} from "./pages";
-import {Header} from "./components";
 import {MainLayout} from "./layouts";
 
 const App = () => {
     return (
         <div>
-            <Header/>
+
     <Routes>
-        <Route> path={'/'} element={<MainLayout/>}
+
+        <Route path={'/'} element={<MainLayout/>}>
         <Route path={'/'} element={<HomePage/>}/>
         <Route path={'todos'} element={<TodosPage/>}/>
         <Route path={'albums'} element={<AlbumsPage/>}/>
         <Route path={'comments'} element={<CommentsPage/>}/>
         </Route>
+
     </Routes>
         </div>
 );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
     return (
         <div className={css.Header}>
             <NavLink to={"/"}>home</NavLink>
-            <NavLink to={"/todos"}>todos</NavLink>
-            <NavLink to={"/albums"}>albums</NavLink>
-            <NavLink to={"/comments"}>comments</NavLink>
+            <NavLink to={"todos"}>todos</NavLink>
+            <NavLink to={"albums"}>albums</NavLink>
+            <NavLink to={"comments"}>comments</NavLink>
         </div>
     );
 };
