b1068c0affc0dd3a9c4ad44279bee1c0bbed5404
Mykhailo525 <mtrindak@gmail.com>
2023-01-29 22:01:25 +0200

    First commit

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useEffect, useState} from "react";
+import {postsService} from "../../services";
 
-const Post = () => {
+const Post = ({postId}) => {
+    const[post,setPost]=useState(null)
+
+    useEffect(()=>{postsService.getById(postId).then(value => value.data).then(value => setPost(value))
+    },[])
     return (
         <div>
-            Post
+            {post &&
+                <>
+                    <div>userId : {post.userId}</div>
+                    <div>id : {post.id}</div>
+                    <div>title : {post.title}</div>
+                    <div>body : {post.body}</div>
+                </>
+            }
         </div>
     );
 };
--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -1,23 +1,14 @@
-import React, {useEffect, useState} from 'react';
+
 import {useParams} from "react-router-dom";
-import {postsService} from "../../services";
+import {Post} from "../../components";
+
 
 const PostPage = () => {
     const {postId}=useParams();
 
-    const[post,setPost]=useState(null)
-
-    useEffect((postId)=>{postsService.getById().then(value => value.data).then(value =>setPost(value))},[])
     return (
         <div>
-            {post &&
-                <>
-                    <div>userId : {post.userId}</div>
-                    <div>id : {post.id}</div>
-                    <div>title : {post.title}</div>
-                    <div>body : {post.body}</div>
-                </>
-            }
+           <Post postId={postId}/>
         </div>
     );
 };
