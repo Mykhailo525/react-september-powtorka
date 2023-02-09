8a723235572d0d7519090170d79d814c67167549
Mykhailo525 <mtrindak@gmail.com>
2023-02-09 16:24:58 +0200

    First commit

--- src/components/Pets/Pets.js
+++ src/components/Pets/Pets.js
@@ -6,28 +6,26 @@ import css from './Pets.module.css'
 const Pets = () => {
 
     const reducer = (state,action) => {
-        switch (action.type){
-            case'ADDDOG':
-                const dogSlice=state.dogs.slice(-1);
-                const dogId=dogSlice.length?dogSlice[0].id+1:0
-                return{...state,dogs: [...state.dogs,{id:dogId,name:action.payload}]}
-            case'ADDCAT':
-                const catSlice=state.cats.slice(-1);
-                const catId=catSlice.length?catSlice[0].id+1:0
-                return{...state,cats: [...state.cats,{id:catId,name:action.payload}]}
+        switch (action.type) {
+            case'ADD_DOG':
+                const dogSlice = state.dogs.slice(-1);
+                const dogId = dogSlice.length ? dogSlice[0].id + 1 : 0
+                return {...state, dogs: [...state.dogs, {id: dogId, name: action.payload}]}
+            case'ADD_CAT':
+                const catSlice = state.cats.slice(-1);
+                const catId = catSlice.length ? catSlice[0].id + 1 : 0
+                return {...state, cats: [...state.cats, {id: catId, name: action.payload}]}
             case'DELETE_DOG_BY_ID':
-                const dogIndex= state.dogs.findIndex(value=>value.id ===action.payload)
-                state.dogs.splice(dogIndex,1)
+                const dogIndex = state.dogs.findIndex(value => value.id === action.payload)
+                state.dogs.splice(dogIndex, 1)
                 return {...state}
             case'DELETE_CAT_BY_ID':
-                const catIndex= state.cats.findIndex(value=>value.id ===action.payload)
-                state.cats.splice(catIndex,1)
+                const catIndex = state.cats.findIndex(value => value.id === action.payload)
+                state.cats.splice(catIndex, 1)
                 return {...state}
         }
 
-
-        }
-
+    }
 
     const initialValue = () => (
         {cats: [], dogs:[]}
@@ -39,13 +37,23 @@ const Pets = () => {
 
     const cat=useRef()
 
+    const createDog = () => {
+        dispatch({type:'ADD_DOG',payload:dog.current.value})
+        dog.current.value=''
+    };
+
+    const createCat = () => {
+        dispatch({type: 'ADD_CAT', payload: cat.current.value})
+        cat.current.value = ''
+    };
+
     return (
         <div>
             <div>
                 <input type="text" placeholder={'dogs'} ref={dog}/>
-                <button onClick={()=>dispatch({type:'ADDDOG',payload:dog.current.value})}>AddDog</button>
+                <button onClick={createDog}>AddDog</button>
                 <input type="text" placeholder={'cats'} ref={cat}/>
-                <button onClick={()=>dispatch({type:'ADDCAT',payload:cat.current.value})}>AddCat</button>
+                <button onClick={createCat}>AddCat</button>
             </div>
 <hr/>
             <div className={css.Pets}>
